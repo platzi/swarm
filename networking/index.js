22cc5990b7e96731d392a0e14b4e27b1bb6a0bb3
Guido Vilariño <gvilarino@gmail.com>
2018-11-16 14:39:39 -0500

    Fix networking project

--- networking/index.js
+++ networking/index.js
@@ -3,22 +3,23 @@ const app = express()
 const port = 3000
 
 const MongoClient = require('mongodb').MongoClient
+const db = 'test'
 
 // Connection URL
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
+const mongoUrl = process.env.MONGO_URL || `mongodb://localhost:27017/${db}`;
 
 app.get('/', (req, res) => {
-  MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
+  MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, client) => {
     if (err) {
       res.status(500).send(`💥 BOOM 💥: ${err}`);
     } else {
-      db.collection('pings')
+      client.db(db).collection('pings')
         .insert({ ip: req.ip, when: +new Date()}, (err, result) => {
           if (err) {
             res.send(`Error al escribir en la DB: ${err}`);
           } else{
             res.send('Me conecté a la DB y dejé constancia');
-            db.close();
+            client.close();
           }
         })
     }
