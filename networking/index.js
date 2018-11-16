cd481ff724b2a62523aeb1db23044e0275533ec4
Guido Vilariño <gvilarino@gmail.com>
2018-11-15 22:58:42 -0500

    Add networking project

---
+++ networking/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const MongoClient = require('mongodb').MongoClient
+
+// Connection URL
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
+
+app.get('/', (req, res) => {
+  MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
+    if (err) {
+      res.status(500).send(`💥 BOOM 💥: ${err}`);
+    } else {
+      db.collection('pings')
+        .insert({ ip: req.ip, when: +new Date()}, (err, result) => {
+          if (err) {
+            res.send(`Error al escribir en la DB: ${err}`);
+          } else{
+            res.send('Me conecté a la DB y dejé constancia');
+            db.close();
+          }
+        })
+    }
+  });
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
